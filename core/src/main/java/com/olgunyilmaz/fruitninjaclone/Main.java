e9f2ce29d266b0daba261119c61e159dfd6971b2
olgun-yilmaz <olgunyilm@gmail.com>
2025-01-25 17:04:16 +0300

    Added random fruit movement: Fruits now fly in random positions and directions

--- core/src/main/java/com/olgunyilmaz/fruitninjaclone/Main.java
+++ core/src/main/java/com/olgunyilmaz/fruitninjaclone/Main.java
@@ -9,8 +9,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Random;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter implements InputProcessor {
@@ -23,8 +27,12 @@ public class Main extends ApplicationAdapter implements InputProcessor {
     Texture ruby;
     BitmapFont font;
 
+    Random random = new Random();
+    Array<Fruit> fruitArray = new Array<>();
+
     int lives = 4;
     int score = 0;
+    int maxDimension;
     private double current_time;
     private double game_over_time = -1.0f;
 
@@ -38,6 +46,9 @@ public class Main extends ApplicationAdapter implements InputProcessor {
         bill = new Texture("bill.png");
         cherry = new Texture("cherry.png");
         ruby = new Texture("ruby.png");
+
+        maxDimension = Math.max(Gdx.graphics.getHeight(),Gdx.graphics.getWidth()); //Landspace or portrait?
+        Fruit.radius = maxDimension / 20f;
 
         Gdx.input.setInputProcessor(this);
 
@@ -59,6 +70,8 @@ public class Main extends ApplicationAdapter implements InputProcessor {
         float deltaTime = (float) frameTime;
         System.out.println("delta time "+deltaTime);
 
+        addItem();
+
         if (lives == 0 && game_over_time == 0f){
             // game over
             game_over_time = current_time;
@@ -70,11 +83,51 @@ public class Main extends ApplicationAdapter implements InputProcessor {
             for (int i = 1; i <= lives; i++){
                 batch.draw(apple,i*75f,Gdx.graphics.getHeight()-50f,apple.getWidth()/20,apple.getHeight()/20);
             }
+
+            for (Fruit fruit : fruitArray){
+                fruit.update(deltaTime);
+
+                Texture texture = apple ; // default case
+
+
+                switch (fruit.type){
+                    case LIFE :
+                        texture = bill;
+                        break;
+                    case ENEMY:
+                        texture = ruby;
+                        break;
+                    case EXTRA:
+                        texture = cherry;
+                        break;
+                }
+
+                batch.draw(texture,fruit.getPos().x,fruit.getPos().y,Fruit.radius,Fruit.radius);
+
+            }
+
         }
 
         font.draw(batch,"SCORE : 0",30,70);
         font.draw(batch,"Cut to play!",(int)(Gdx.graphics.getWidth()/2.5),Gdx.graphics.getHeight()/2);
         batch.end();
+    }
+
+    private void addItem(){
+        float pos = random.nextFloat() * maxDimension;
+        Fruit item = new Fruit(new Vector2(pos,-Fruit.radius), new Vector2(
+            (Gdx.graphics.getWidth()*.5f)+random.nextFloat(),(Gdx.graphics.getHeight()*.5f)));
+
+        float type = random.nextFloat();
+        if (type > 0.98){ // bill ->%2
+            item.type = Fruit.Type.LIFE;
+        }else if(type > 0.88){ //cherry -> % 10
+            item.type = Fruit.Type.EXTRA;
+        }else if(type > 0.75){ // enemy -> % 13
+            item.type = Fruit.Type.ENEMY;
+        }
+
+        fruitArray.add(item);
     }
 
     @Override
