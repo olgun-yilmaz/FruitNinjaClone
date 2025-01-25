4a1baef2331f62e1164215096b027c3fac1307f6
olgun-yilmaz <olgunyilm@gmail.com>
2025-01-25 09:29:04 +0300

    Added font and InputProcessor integration

--- core/src/main/java/com/olgunyilmaz/fruitninjaclone/Main.java
+++ core/src/main/java/com/olgunyilmaz/fruitninjaclone/Main.java
@@ -2,21 +2,41 @@ package com.olgunyilmaz.fruitninjaclone;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
+public class Main extends ApplicationAdapter implements InputProcessor {
+    // for user input handle
     SpriteBatch batch;
     Texture background;
+    Texture apple;
+    Texture bill;
+    Texture cherry;
+    Texture ruby;
+    BitmapFont font;
 
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         background = new Texture("ninjabackground.png");
+        apple = new Texture("apple.png");
+        bill = new Texture("bill.png");
+        cherry = new Texture("cherry.png");
+        ruby = new Texture("ruby.png");
+
+        Gdx.input.setInputProcessor(this);
+
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        font.getData().setScale(4);
+
 
     }
 
@@ -24,12 +44,60 @@ public class Main extends ApplicationAdapter {
     public void render() {
         batch.begin();
         batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        font.draw(batch,"SCORE : 0",30,70);
+        font.draw(batch,"Cut to play!",(int)(Gdx.graphics.getWidth()/2.5),Gdx.graphics.getHeight()/2);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        background.dispose();
+        font.dispose();
+
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
     }
 }
